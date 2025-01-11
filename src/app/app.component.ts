d9f23ec28dbf87adcc9f56b73360a92fd702550c
hesham <heshamali1995@hotmail.com>
2025-01-11 18:42:57 +0200

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,15 @@ export class AppComponent {
     } else {
       console.log('fallback');
       window.alert('fallback happens');
+      navigator
+        .share({
+          title: title,
+          url: productURL,
+        })
+        .catch((error: any) => {
+          console.log('error', error);
+          window.alert(`error happens ${error}`);
+        });
     }
   };
 }
