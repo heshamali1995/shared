b7b2772e76affe24b5361626ff8231148f6dc18c
hesham <heshamali1995@hotmail.com>
2025-01-11 18:25:26 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myapp';
+  fallback = false;
+  onShareMobile = () => {
+    let productURL = window.location.href;
+    let title = document.title;
+    if (typeof navigator.share === 'function') {
+      navigator
+        .share({
+          title: title,
+          url: productURL,
+        })
+        .catch((error: any) => {
+          console.log('error', error);
+          window.alert(`error happens ${error}`);
+        });
+    } else {
+      console.log('fallback');
+      window.alert('fallback happens');
+    }
+  };
 }
